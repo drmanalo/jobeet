84d42895fd5482d12139f40b16b652d5cc34dddb
drmanalo <drmanalo@hotmail.co.uk>
2013-11-24 10:25:05 +0000

    Added atom feed

--- src/Ibw/JobeetBundle/Repository/JobRepository.php
+++ src/Ibw/JobeetBundle/Repository/JobRepository.php
@@ -88,4 +88,29 @@ class JobRepository extends EntityRepository {
 	
 	}
 	
+	public function getLatestPost($category_id = null) {
+	
+		$query = $this->createQueryBuilder('j')
+		->where('j.expires_at > :date')
+		->setParameter('date', date('Y-m-d H:i:s', time()))
+		->andWhere('j.is_activated = :activated')
+		->setParameter('activated', 1)
+		->orderBy('j.expires_at', 'DESC')
+		->setMaxResults(1);
+	
+		if($category_id) {
+			$query->andWhere('j.category = :category_id')
+			->setParameter('category_id', $category_id);
+		}
+	
+		try{
+			$job = $query->getQuery()->getSingleResult();
+		} catch(\Doctrine\Orm\NoResultException $e){
+			$job = null;
+		}
+	
+		return $job;
+		 
+	}
+	
 }
